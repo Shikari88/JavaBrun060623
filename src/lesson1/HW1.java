8c7745195f961ce876ce6278d20ba7064cb3c623
Shikari88 <astakhin.rsm@mail.ru>
2023-06-08 10:43:28 +0300

    add task Easy for HW1

---
+++ src/lesson1/HW1.java
@@ -0,0 +1,23 @@
+package lesson1;
+
+//Easy Создайте метод, который принимает на ввод
+// с клавиатуры два числа и возвращает их сумму
+
+import java.util.Scanner;
+
+public class HW1 {
+    public static int getSum() {
+        System.out.println("Введите два числа");
+        Scanner scanner = new Scanner(System.in);
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        scanner.close();
+        return num1 + num2;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Сумма равна: " + getSum());
+
+    }
+}
